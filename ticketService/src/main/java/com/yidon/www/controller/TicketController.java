40d9ad1466e5ce1368f9fab08c751ba67d420728
itcast <hello@itcast.cn>
2023-01-07 22:22:30 +0800

    车次表增删改查

--- ticketService/src/main/java/com/yidon/www/controller/TicketController.java
+++ ticketService/src/main/java/com/yidon/www/controller/TicketController.java
@@ -1,11 +1,12 @@
 package com.yidon.www.controller;
 
 import com.yidon.www.common.Result;
+import com.yidon.www.pojo.Ticket;
 import com.yidon.www.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author wuLinTao
@@ -18,4 +19,58 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
+    @PostMapping("/add")
+    public Result add(@RequestBody Ticket ticket) {
+        try {
+            ticketService.add(ticket);
+            return Result.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("新增失败");
+        }
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public Result deleteById(@PathVariable(value = "id") Integer id) {
+        try {
+            ticketService.deleteById(id);
+            return Result.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("删除失败");
+        }
+    }
+
+    @PutMapping("/updateById/{id}")
+    public Result updateById(@RequestBody Ticket ticket) {
+        try {
+            ticketService.updateById(ticket);
+            return Result.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("更新失败");
+        }
+    }
+
+    @GetMapping("/selectById/{id}")
+    public Result selectById(@PathVariable(value = "id") Integer id) {
+        try {
+            Ticket ticket = ticketService.selectById(id);
+            return Result.success(ticket);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("查询用户数据失败");
+        }
+    }
+
+    @GetMapping("/selectAll")
+    public Result selectAll() {
+        try {
+            List<Ticket> list = ticketService.selectAll();
+            return Result.success(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.fail("查询失败");
+        }
+    }
 }
--- ticketService/src/main/java/com/yidon/www/service/TicketService.java
+++ ticketService/src/main/java/com/yidon/www/service/TicketService.java
@@ -1,11 +1,23 @@
 package com.yidon.www.service;
 
-import com.yidon.www.common.Result;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yidon.www.pojo.Ticket;
+
+import java.util.List;
 
 /**
  * @author wuLinTao
  * 时间：2023-01-06 12:33
  */
-public interface TicketService {
+public interface TicketService extends IService<Ticket> {
 
+    void add(Ticket ticket);
+
+    void deleteById(Integer id);
+
+    boolean updateById(Ticket ticket);
+
+    Ticket selectById(Integer id);
+
+    List<Ticket> selectAll();
 }
