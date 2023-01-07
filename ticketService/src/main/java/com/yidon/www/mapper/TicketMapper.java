8293438285e731406db050dfd6ac471c838029cc
itcast <hello@itcast.cn>
2023-01-07 22:22:30 +0800

    车次表增删改查

--- ticketService/src/main/java/com/yidon/www/mapper/TicketMapper.java
+++ ticketService/src/main/java/com/yidon/www/mapper/TicketMapper.java
@@ -1,11 +1,17 @@
 package com.yidon.www.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yidon.www.pojo.Ticket;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author wuLinTao
  * 时间：2023-01-06 12:33
  */
 @Repository
-public interface TicketMapper {
+public interface TicketMapper extends BaseMapper<Ticket> {
+
 }
