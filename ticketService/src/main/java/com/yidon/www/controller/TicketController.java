a210fe2f77a5df7685a3aae2a5e937991251e17c
JerryC2001 <947379598@qq.com>
2023-01-08 22:51:04 +0800

    fix ticketService的master分支

--- ticketService/src/main/java/com/yidon/www/controller/TicketController.java
+++ ticketService/src/main/java/com/yidon/www/controller/TicketController.java
@@ -1,13 +1,25 @@
 package com.yidon.www.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.yidon.www.common.Result;
-import com.yidon.www.constant.HttpConstant;
-import com.yidon.www.pojo.Ticket;
-import com.yidon.www.service.TicketService;
+import com.yidon.www.pojo.OrderTicketInfo;
+import com.yidon.www.pojo.ProductTrainInfo;
+import com.yidon.www.pojo.SysUserInfo;
+import com.yidon.www.service.OrderTicketInfoService;
+import com.yidon.www.service.ProductTrainInfoService;
+import com.yidon.www.service.SysUserInfoService;
+import com.yidon.www.vo.OrderTicketInfoAddVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+
+import static com.yidon.www.constant.HttpConstant.HTTP_ERROR;
 
 /**
  * @author wuLinTao
@@ -16,59 +28,99 @@ import java.util.List;
 @RestController
 @RequestMapping("/ticket")
 public class TicketController {
-
     @Autowired
-    private TicketService ticketService;
+    private OrderTicketInfoService orderTicketInfoService;
+    @Autowired
+    private SysUserInfoService sysUserInfoService;
+    @Autowired
+    private ProductTrainInfoService productTrainInfoService;
 
-    @PostMapping("/add")
-    public Result add(@RequestBody Ticket ticket) {
-        try {
-            ticketService.add(ticket);
-            return Result.success();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Result.fail(HttpConstant.HTTP_FAIL, "新增失败");
+    // 查询用户车票
+    @GetMapping("/searchTicket/{id}")
+    private Result searchTicket(@PathVariable Integer id){
+        QueryWrapper<OrderTicketInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_id", id);
+        List<OrderTicketInfo> list = orderTicketInfoService.list(queryWrapper);
+        return Result.success(list);
+    }
+
+    // 购买车票
+    @PostMapping("/addTicket")
+    private Result addTicket(@RequestBody OrderTicketInfoAddVO orderTicketInfoAddVO) throws ParseException {
+        OrderTicketInfo orderTicketInfo = new OrderTicketInfo();
+        // 检查用户是否为有效用户
+        Integer user_id = orderTicketInfoAddVO.getUser_id();
+        SysUserInfo sysUserInfo = sysUserInfoService.getById(user_id);
+        if (sysUserInfo == null){
+            return Result.fail("用户无效");
+        }
+        // 检查用户是否已经购买该车票
+        QueryWrapper<OrderTicketInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("train_id", orderTicketInfoAddVO.getTrain_id());
+        queryWrapper.eq("user_id", orderTicketInfoAddVO.getUser_id());
+        OrderTicketInfo one = orderTicketInfoService.getOne(queryWrapper, false);
+        if (one != null){
+            return Result.fail("该用户已购买该车票！");
+        }
+        // 用户有效，设置用户信息到orderTicketInfo里面
+        orderTicketInfo.setUserId(sysUserInfo.getUserId());
+        orderTicketInfo.setUserName(sysUserInfo.getUserName());
+        // 检查车次信息是否有效
+        Integer train_id = orderTicketInfoAddVO.getTrain_id();
+        ProductTrainInfo productTrainInfo = productTrainInfoService.getById(train_id);
+        if (productTrainInfo == null){
+            return Result.fail("车次不存在");
+        }
+        // 车次有效，将车次信息设置到orderTicketInfo里面
+        orderTicketInfo.setArrStation(productTrainInfo.getArrStation());
+        orderTicketInfo.setDpStation(productTrainInfo.getDpStation());
+        orderTicketInfo.setLpNum(productTrainInfo.getLpNum());
+        orderTicketInfo.setTrainId(productTrainInfo.getId());
+        orderTicketInfo.setPrice(productTrainInfo.getPrice());
+        // 设置时间
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd 23:59:59");
+        Date date = new Date(1592409600000L);
+        String d1= simpleDateFormat.format(date);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date res = format.parse(d1);
+        orderTicketInfo.setCreateTime(res);
+        orderTicketInfo.setUpdateTime(res);
+        orderTicketInfo.setIs_deleted(0);
+
+        // 检查是否还有剩余车票
+        if (productTrainInfo.getCap() > 0){
+            productTrainInfo.setCap(productTrainInfo.getCap() - 1);
+            productTrainInfoService.updateById(productTrainInfo);
+            orderTicketInfoService.save(orderTicketInfo);
+            return Result.success("成功购票");
+        }else {
+            return Result.fail("票数不足！");
         }
     }
 
-    @DeleteMapping("/deleteById/{id}")
-    public Result deleteById(@PathVariable(value = "id") Integer id) throws RuntimeException{
-        Ticket ticket = ticketService.selectById(id);
-        if (ticket != null) {
-            ticketService.deleteById(id);
-            return Result.success(ticket);
-        } else {
-            return Result.fail(HttpConstant.HTTP_FAIL, "删除失败,该用户不存在");
+    // 退回车票
+    @PostMapping("/deleteTicket/{id}")
+    @Transactional(rollbackFor = Exception.class)
+    public Result deleteTicket(@PathVariable("id") Integer id) {
+        OrderTicketInfo orderTicketInfo = orderTicketInfoService.getById(id);
+        if (orderTicketInfo == null) {
+            return Result.fail("没有当前车票信息，请刷新重新查看");
         }
-    }
+        //TODO 用户校验
+        Integer userId = orderTicketInfo.getUserId();
 
-    @PutMapping("/updateById/{id}")
-    public Result updateById(@RequestBody Ticket ticket) throws RuntimeException{
-        boolean res = ticketService.updateById(ticket);
-        if (res != false) {
-            return Result.success(ticket);
-        } else {
-            return Result.fail(HttpConstant.HTTP_FAIL, "更新失败，该用户不存在");
+        Integer trainId = orderTicketInfo.getTrainId();
+        ProductTrainInfo productTrainInfo = productTrainInfoService.getById(trainId);
+        if (productTrainInfo == null) {
+            return Result.fail("没有当前车次信息，请重试");
         }
-    }
-
-    @GetMapping("/selectById/{id}")
-    public Result selectById(@PathVariable(value = "id") Integer id) throws RuntimeException {
-        Ticket ticket = ticketService.selectById(id);
-        if (ticket != null) {
-            return Result.success(ticket);
-        } else {
-            return Result.fail(HttpConstant.HTTP_FAIL, "查询失败，该用户不存在");
+        productTrainInfo.setTicketCount(productTrainInfo.getTicketCount() + 1);
+        orderTicketInfo.setUpdateTime(new Date());
+        boolean result = productTrainInfoService.updateById(productTrainInfo);
+        result &= orderTicketInfoService.removeById(orderTicketInfo);
+        if (!result) {
+            return Result.fail(HTTP_ERROR, "数据库操作失败，请联系管理员");
         }
-    }
-
-    @GetMapping("/selectAll")
-    public Result selectAll() throws RuntimeException {
-        List<Ticket> list = ticketService.selectAll();
-        if (list != null) {
-            return Result.success(list);
-        } else {
-            return Result.fail(HttpConstant.HTTP_FAIL, "查询失败，系统无数据");
-        }
+        return Result.success("退票成功");
     }
 }
