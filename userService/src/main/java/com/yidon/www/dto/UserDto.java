7fd32d003d73e5df5714c2fcc77f2aa529f66746
BXuan <ybxuan2002@163.com>
2023-01-09 11:26:40 +0800

    车票接口第二次push

---
+++ userService/src/main/java/com/yidon/www/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.yidon.www.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zhongxuetao
+ * @Description
+ * @date
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String loginName;
+
+    private String userName;
+
+    private String password;
+}
